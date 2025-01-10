341fa6ca66444f342a966546984231845d069122
toshmuhammad <toshmuhammad@gmail.com>
2025-01-10 13:12:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+let = 1
+
+let k = 7
+let n = 5
+
+for (let i = 0; i < n; i++) {
+    console.log(i, k);
+}
+
+// For 2
+
+// let narx = 10000
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(`${i / 10} kg = ${+((i / 10) * narx).toFixed(2)} so'm`);
+// }
+
+// For 6
+
+// let son = 50
+
+// for (let i = 1; i <= son; i++) {
+//     if (50 % i == 0)
+//     console.log(i);
+// }
+
+
+
+// For 1
+
+// let a = 10
+// let b = 20
+
+// for (let i = a; i <= b; i++) {
+//     console.log(i);
+// }
+
+// For 2
+
+// let num = +prompt('son kiriting')
+// for (let i = num; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// For 3
+
+// let narx = 5000
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(`${i} kg = ${+((i) * narx).toFixed(2)} so'm`);
+// }
+
+// For 4
+
+// let narx = 5000
+
+// for (let i = 12; i < 21; i++) {
+//     console.log(`${i / 10} kg = ${+((i / 10) * narx).toFixed(2)} so'm`);
+// }
+
